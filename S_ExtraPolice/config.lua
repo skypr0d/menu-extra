d6581b5946b7309c64406addd7363909342c5c96
Skypr0d <67479263+skypr0d@users.noreply.github.com>
2023-02-18 19:10:29 +0100

    Update config.lua

--- S_ExtraPolice/config.lua
+++ S_ExtraPolice/config.lua
@@ -1,6 +1,6 @@
 ConfigModifVehicleSAPD = {
     Main = {
-        Position = vector3(-1077.62, -845.37, 4.88),
+        Position = vector3(-1077.62, -845.37, 4.88), -- client.lua, lines 141, modify the position if you change this one
         Text3D = "[~b~E~s~] Ouvrir",
         Distance = 8,
         Marker = {dirX = 0.0, dirY = 0.0, dirZ = 0.0, rotX = -90.0, rotY = 145.0, rotZ = 0.0, scaleX = 1.5, scaleY = 1.5, scaleZ = 1.5},
